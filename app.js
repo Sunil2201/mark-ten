dffe6db8ebfd79da2f9ad28e611bff718adc6567
Sunil Ballani <ballanisunil123@gmail.com>
2022-09-07 00:39:55 +0530

    initialized constants

---
+++ app.js
@@ -0,0 +1,11 @@
+const billAmount = document.querySelector("#billAmount");
+const cashGiven = document.querySelector('#cashGiven');
+const checkButton = document.querySelector("#checkButton");
+const nextButton = document.querySelector('#nextButton');
+const message = document.querySelector("#errorMessage");
+const noOfNotes = document.querySelectorAll("#noteCount");
+const inputCashSection = document.querySelector(".inputCash");
+const outputSection = document.querySelector(".outputSection");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+
